3b54f435a0515dc373c3a761e5ad9c91bf282c1b
Maciej Sulecki <sulecki.maciek@gmail.com>
2022-03-23 12:28:42 +0000

    hopefully working!!!!!

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,48 @@
 import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { DirectiveLocation, GraphQLDirective } from "graphql";
+import {
+  GraphQLSchemaBuilderModule,
+  GraphQLSchemaFactory,
+} from "@nestjs/graphql";
 
+import { DateScalar } from "./common/scalars/date.scalar";
 import { GraphQLModule } from "@nestjs/graphql";
 import { Module } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
 import { RecipesModule } from "./recipes/recipes.module";
+import { RecipesResolver } from "./recipes/recipes.resolver";
 import { upperDirectiveTransformer } from "./common/directives/upper-case.directive";
+
+async function generateSchema() {
+  const app = await NestFactory.create(GraphQLSchemaBuilderModule);
+  await app.init();
+
+  const gqlSchemaFactory = app.get(GraphQLSchemaFactory);
+  const schema = await gqlSchemaFactory.create(
+    [RecipesResolver],
+    [DateScalar],
+    {
+      directives: [
+        new GraphQLDirective({
+          name: "upper",
+          locations: [DirectiveLocation.FIELD_DEFINITION],
+        }),
+      ],
+    }
+  );
+  return schema;
+}
 
 @Module({
   imports: [
     RecipesModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: "schema.gql",
-      transformSchema: (schema) => upperDirectiveTransformer(schema, "upper"),
       installSubscriptionHandlers: true,
-      buildSchemaOptions: {
-        directives: [
-          new GraphQLDirective({
-            name: "upper",
-            locations: [DirectiveLocation.FIELD_DEFINITION],
-          }),
-        ],
+      autoSchemaFile: false,
+      transformSchema: async () => {
+        const schema = await generateSchema();
+        return upperDirectiveTransformer(schema, "upper");
       },
     }),
   ],
