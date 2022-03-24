5374546963a0be55a1ee143ee32fe5ac611fbb63
Maciej Sulecki <sulecki.maciek@gmail.com>
2022-03-24 00:44:51 +0000

    trying

--- api/main.ts
+++ api/main.ts
@@ -3,17 +3,12 @@ import * as dree from "dree";
 import { AppModule } from "../src/app.module";
 import { NestFactory } from "@nestjs/core";
 import { ValidationPipe } from "@nestjs/common";
-import { join } from "path";
-import { readFileSync } from "fs";
 
 export default async function bootstrap() {
-  const string = dree.parse(__dirname, {
+  const string = dree.parse(process.cwd(), {
     exclude: [/node_modules/, /recipes/, /common/],
   });
   console.log(string);
-
-  const file = readFileSync(join(__dirname, "schema.gql"), "utf8");
-  console.log(file);
 
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
